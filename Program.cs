63b2d9986a1ae8f0abc218d0bc2ba406053ef164
Minsetr0 <nesterovmaksim71@mail.ru>
2023-04-28 19:52:39 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿int[] numbers = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, };
+int tempNumber = 0;
+int firstNumber = numbers[0];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (i != numbers.Length - 1)
+    {   
+        tempNumber = numbers[i + 1];
+        numbers[i] = tempNumber;
+    }
+    else
+    {
+        numbers[i] = 0;
+    }
+}
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.WriteLine(numbers[i] + " ");
+}
