5a22dddc3955496657a0111170148b3a656c796a
Minsetr0 <nesterovmaksim71@mail.ru>
2023-05-03 23:00:30 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,17 +1,31 @@
-﻿int[] numbers = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, };
+﻿int[] numbers = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 int tempNumber = 0;
 int firstNumber = numbers[0];
 
+Console.Write("Дан массив: ");
+
 for (int i = 0; i < numbers.Length; i++)
 {
-    if (i != numbers.Length - 1)
-    {   
-        tempNumber = numbers[i + 1];
-        numbers[i] = tempNumber;
-    }
-    else
+    Console.Write(numbers[i] + " ");
+}
+
+Console.WriteLine("\nНа сколько позиций влево его сдвинуть?");
+
+int shiftRepetitions = Console.Read();
+
+for (int i = 0; i < shiftRepetitions; i++)
+{ 
+    for (int j = 0; j < numbers.Length; j++)
     {
-        numbers[i] = 0;
+        if (j != numbers.Length - 1)
+        {
+            tempNumber = numbers[j + 1];
+            numbers[j] = tempNumber;
+        }
+        else
+        {
+            numbers[j] = 0;
+        }
     }
 }
 
