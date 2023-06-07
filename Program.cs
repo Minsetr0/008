5e58d34a4f4bdcb694c32894c01218bac4b1f41d
Minsetr0 <nesterovmaksim71@mail.ru>
2023-06-07 13:56:37 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿int[] numbers = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 int tempNumber = 0;
-int firstNumber = numbers[0];
 
 Console.Write("Дан массив: ");
 
@@ -15,13 +14,14 @@ int shiftRepetitions = Console.Read();
 
 for (int i = 0; i < shiftRepetitions; i++)
 {
-    tempNumber = numbers[numbers.Length - 1];
+    tempNumber = numbers[0];
 
-    for (int j = numbers.Length - 1; j > 0; j--)
+    for (int j = 0; j < numbers.Length - 1; j++)
     {
-        numbers[j] = numbers[j - 1];
+        numbers[j] = numbers[j + 1];
     }
-    numbers[0] = tempNumber;
+
+    numbers[numbers.Length - 1] = tempNumber;
 }
 
 for (int i = 0; i < numbers.Length; i++)
